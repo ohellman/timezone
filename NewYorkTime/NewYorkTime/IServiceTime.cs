b9f6006d2793ba063f63167a63730754b254117f
svcTfsSetup_trv <tfs-frvaltningen@trafikverket.se>
2015-12-22 15:19:24 +0100

    Forgot the solution

---
+++ NewYorkTime/NewYorkTime/IServiceTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace NewYorkTime
+{
+    [ServiceContract]
+    public interface IServiceTime
+    {
+
+        [OperationContract]
+        DateTime NewYorkTime();
+        
+    }
+}
---
+++ NewYorkTime/NewYorkTime/ServiceTime.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace NewYorkTime
+{
+    public class ServiceTime : IServiceTime
+    {
+        public DateTime NewYorkTime()
+        {
+            return DateTime.Now.AddHours(-6);
+        }
+
+        public DateTime SanFranciscoTime()
+        {
+            return DateTime.Now.AddHours(-8);
+        }
+
+        public DateTime KoreaTime()
+        {
+            return DateTime.Now.AddHours(+9);
+        }
+    }
+}
